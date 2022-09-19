202540ab61abde71611e705501dc2118b0db9ba2
Mohammed Falion <111682711+m-f-2000@users.noreply.github.com>
2022-09-19 11:55:52 +0300

    Fixing layer issue + adding spline mesh

--- Assets/_Scripts/WaterController.cs
+++ Assets/_Scripts/WaterController.cs
@@ -9,18 +9,22 @@ public class WaterController : MonoBehaviour
 {
 
     [SerializeField] private Grass grassPrefab;
-    [SerializeField] private LayerMask layerMask;
     [SerializeField] private Transform grassParent;
     [SerializeField] private int maxWater;
     [SerializeField] private Image waterBar;
 
 
     private int currentWater;
+    private int LayersToHit;
 
 
     private void Awake()
     {
         currentWater = maxWater;
+
+        var groundLayer = 1 << LayerMask.NameToLayer("Ground");
+        var wellLayer = 1 << LayerMask.NameToLayer("Well");
+        LayersToHit = groundLayer | wellLayer;
     }
 
 
@@ -28,29 +32,23 @@ public class WaterController : MonoBehaviour
     {
         if (!Input.GetMouseButtonDown(0)) return;
 
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var _hit, float.MaxValue, LayersToHit)) return;
 
-        RaycastHit _hit;
-        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out _hit, float.MaxValue, layerMask)) return;
 
         if (_hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground") && currentWater > 0)          // if it hits the ground
         {
+            Debug.Log("X");
             Grass grass = Instantiate(grassPrefab, _hit.point, Quaternion.identity, grassParent);
             currentWater -= 1;
             waterBar.fillAmount = currentWater / (float)maxWater;
         }
 
         // TODO there is the error
-        if (_hit.collider.gameObject.layer == LayerMask.NameToLayer("Well"))                           // if it hits the well
+        if (_hit.collider.gameObject.layer == LayerMask.NameToLayer("Well"))                                // if it hits the well
         {
             currentWater = maxWater;
             waterBar.fillAmount = currentWater / (float)maxWater;
         }
     }
-
-    public void FillWell(BaseEventData baseEventData)
-    {
-        currentWater = 5;
-    }
-
 
 }
