4764082423aeb703b9f9e98118e51e9381af5d69
Mohammed Falion <111682711+m-f-2000@users.noreply.github.com>
2022-09-21 15:16:13 +0300

    Fix Nav

---
+++ Assets/_Scripts/EnemyAI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Threading.Tasks;
+
+public enum EnemyState { Patrolling, Chasing, Attaking }
+
+
+public class EnemyAI : MonoBehaviour
+{
+    
+}
